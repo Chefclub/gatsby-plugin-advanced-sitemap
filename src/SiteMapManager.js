171f5c9205f804e022831edaf69c0b9d220d3aea
Alexandre Fauchard <alexandre.fauchard@toolpad.fr>
2022-01-21 17:43:41 +0100

    Some tests

--- src/SiteMapManager.js
+++ src/SiteMapManager.js
@@ -1,59 +1,88 @@
-import SiteMapIndexGenerator from './IndexMapGenerator';
-import SiteMapGenerator from './SiteMapGenerator';
-import _ from 'lodash';
+import SiteMapIndexGenerator from "./IndexMapGenerator"
+import SiteMapGenerator from "./SiteMapGenerator"
+import _ from "lodash"
 
 export default class SiteMapManager {
-    constructor(options) {
-        let sitemapTypes = [];
+  constructor(options, language) {
+    let sitemapTypes = []
 
-        options = options || {};
+    console.log(`options.mapping 1 `, Object.keys(options.mapping))
 
-        this.options = options;
+    options =
+      {
+        ...options,
+        mapping: filterMappingByLanguage(options.mapping, language),
+      } || {}
 
-        for (let type in options.mapping) {
-            const sitemapType = options.mapping[type].sitemap || `pages`;
-            sitemapTypes.push(sitemapType);
-        }
+    console.log(`options.mapping 2 `, Object.keys(options.mapping))
 
-        // ensure, we have a cleaned up array
-        sitemapTypes = _.uniq(sitemapTypes);
+    this.options = options
 
-        // create sitemaps for each type
-        sitemapTypes.forEach((type) => {
-            this[type] = options[type] || this.createSiteMapGenerator(options, type);
-        });
-
-        this.index = options.index || this.createIndexGenerator(sitemapTypes);
-        // create the default pages one for all fallback sitemap URLs
-        this.pages = options.pages || this.createSiteMapGenerator(options, `pages`);
+    for (let type in options.mapping) {
+      console.log(`type`, type)
+      const sitemapType = options.mapping[type].sitemap || `pages`
+      sitemapTypes.push(sitemapType)
     }
 
-    createIndexGenerator(sitemapTypes) {
-        const types = {};
+    // ensure, we have a cleaned up array
+    sitemapTypes = _.uniq(sitemapTypes)
 
-        sitemapTypes.forEach(type => types[type] = this[type]);
+    // create sitemaps for each type
+    sitemapTypes.forEach(type => {
+      this[type] = options[type] || this.createSiteMapGenerator(options, type)
+    })
 
-        return new SiteMapIndexGenerator({
-            types: types
-        });
+    console.log(`options.localesSM`, options.localesSM)
+
+    this.index = options.index || this.createIndexGenerator(sitemapTypes)
+
+    // create the default pages one for all fallback sitemap URLs
+    this.pages = options.pages || this.createSiteMapGenerator(options, `pages`)
+    console.log(`this keys`, Object.keys(this))
+  }
+
+  createIndexGenerator(sitemapTypes) {
+    const types = {}
+
+    sitemapTypes.forEach(type => (types[type] = this[type]))
+
+    return new SiteMapIndexGenerator({
+      types: types,
+    })
+  }
+
+  createSiteMapGenerator(options, type) {
+    return new SiteMapGenerator(options, type)
+  }
+
+  getIndexXml(options) {
+    return this.index.getXml(options)
+  }
+
+  getSiteMapXml(type, options) {
+    if (this[type]) {
+      console.log(`adding ${url} to ${type}`)
+      return this[type].getXml(options)
     }
+  }
 
-    createSiteMapGenerator(options, type) {
-        return new SiteMapGenerator(options, type);
+  // This is the equivalent of adding the URLs on bootstrap by listening to the events
+  // like we do in Ghost core
+  addUrls(type, { url, node }) {
+    if (this[type]) {
+      console.log(`adding ${url} to ${type}`)
+      return this[type].addUrl(url, node)
     }
-
-    getIndexXml(options) {
-        return this.index.getXml(options);
-    }
-
-    getSiteMapXml(type, options) {
-        return this[type].getXml(options);
-    }
-
-    // This is the equivalent of adding the URLs on bootstrap by listening to the events
-    // like we do in Ghost core
-    addUrls(type, {url, node}) {
-        return this[type].addUrl(url, node);
-    }
+  }
 }
 
+const filterMappingByLanguage = (mapping, language) => {
+  let newMapping = {}
+  Object.keys(mapping).forEach(mappingKey => {
+    console.log(mapping[mappingKey].language, language)
+    if (mapping[mappingKey].language === language) {
+      newMapping[mappingKey] = mapping[mappingKey]
+    }
+  })
+  return newMapping
+}
